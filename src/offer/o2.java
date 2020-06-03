eefaa0a3e03bd2041687a3c0cb2182c7bf819f1b
Jason <jason_liujiang@163.com>
2020-06-03 20:24:25 +0800

    java并发实战

--- src/offer/o2.java
+++ src/offer/o2.java
@@ -73,6 +73,7 @@ class Singleton4 {
     //这种写法在getSingleton方法中对singleton进行了两次判空，第一次是为了不必要的同步，第二次是在singleton等于null的情况下才创建实例。
     public static Singleton4 getInstance() {
         if (instance == null) {
+            //类对象加锁
             synchronized (Singleton4.class) {
                 if (instance == null) {
                     instance = new Singleton4();
