137f18e9494a6eeaf75b88d600bc52ea0c2d47fc
liujiang <liujiang@baijia.com>
2021-08-12 11:09:50 +0800

    add

--- src/leetcode/ListNode.java
+++ src/leetcode/ListNode.java
@@ -1,10 +1,31 @@
 package leetcode;
 
-public class ListNode {
-    int val;
-    ListNode next = null;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 
-    public ListNode(int val) {
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
         this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
